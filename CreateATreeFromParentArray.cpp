40ca9cb08482a5fb44f618a9d67353eb297bc350
PranavKumar27 <pranav.jenus@gmail.com>
2023-12-11 00:59:17 +0530

    Update CreateATreeFromParentArray.cpp
    Optimized Approach in BigO(n)

--- CreateATreeFromParentArray.cpp
+++ CreateATreeFromParentArray.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <Map>
 
 using namespace std;
 
@@ -20,6 +22,7 @@ public:
     }
 };
 
+// Done in BigO(N*N)
 void createNode(int parent[],int i,Node* created[],Node** root)
 {
     if(created[i]!=NULL)
@@ -50,6 +53,58 @@ void createNode(int parent[],int i,Node* created[],Node** root)
 
 }
 
+// Done in BigO(N)
+Node* createTree(vector<int>& parent)
+{
+    int n=parent.size();
+    map<int,Node*> Mp;
+    Node* root;
+    for(int i=0;i<n;i++)
+    {
+        if(parent[i]==-1)
+        {
+            root=new Node(i);
+            Mp[i] = root;
+        }
+        else
+        {
+            Node* temp = new Node(i);
+            Mp[i] = temp;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(parent[i]==-1)
+        {
+            continue;
+        }
+        else
+        {
+            auto prnt= Mp.find(parent[i]); // parent
+            if(prnt != Mp.end())
+            {
+                Node* tempParent = prnt->second; // parent
+                auto chd= Mp.find(i); // child
+                if(chd!=Mp.end())
+                {
+                    if(tempParent->left == NULL)
+                    {
+                        tempParent->left = chd->second;
+                    }
+                    else
+                    {
+                        tempParent->right = chd->second;
+                    }
+                }
+
+             }
+
+        }
+    }
+    return root;
+}
+
+
 
 void inOrder(Node* root)
 {
@@ -78,6 +133,11 @@ int main()
 
     inOrder(root);
 
+    cout << endl;
+    vector<int>parent1{1,5,5,2,2,-1,3};
+    Node* root1=createTree(parent1);
+
+    inOrder(root1);
 
     return 0;
 }
